b0d58709122492ab801d03d7dbc8575c091ac6a8
FumiakiC <cheery.wood5574@fastmail.com>
2026-01-10 12:26:41 +0900

    コンテナ環境構築作業

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+
+  output: "standalone", // スタンドアロン出力を有効化
   
   typescript: {
-    ignoreBuildErrors: true,　// ビルド時のTypeScriptエラーを無視
+    ignoreBuildErrors: true, // ビルド時のTypeScriptエラーを無視
   },
   images: {
     unoptimized: true,
